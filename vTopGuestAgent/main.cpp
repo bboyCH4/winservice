c1b6be95bddb6572c4be2201398c3637fe7236f3
chi <hechi1014@126.com>
2017-01-30 11:14:07 +0800

    增加了luanchapp

--- vTopGuestAgent/main.cpp
+++ vTopGuestAgent/main.cpp
@@ -1,4 +1,6 @@
 #include <windows.h>
+#include <Wtsapi32.h>
+#include <Userenv.h>
 #include "LogModule.h"
 
 static void ServiceUninstall();
@@ -6,10 +8,39 @@ static void ServiceMain(int argc, char** argv);
 static void ServiceControlHandler(DWORD request);
 static void ServiceControlManagerUpdate(DWORD dwExitCode, DWORD dwState);
 static BOOL ServiceInit();
+static void LaunchAppInSession(LPSTR lpCommand);
+
+static void AddFeature(struct watch_feature_set* wfs, 
+						const char *path, 
+						const char *flag, 
+						const char *name,
+						void(*handler)(void *), 
+						void *ctx);
 
 static SERVICE_STATUS				_ServiceStatus;
 static SERVICE_STATUS_HANDLE		_hStatus;
 static HANDLE						_hServiceExitEvent;
+
+
+
+
+struct watch_feature 
+{
+	struct watch_event *watch;
+	const char *feature_flag;
+	const char *name;
+	void(*handler)(void *);
+	void *ctx;
+};
+
+#define MAX_FEATURES 10
+struct watch_feature_set 
+{
+	struct watch_feature features[MAX_FEATURES];
+	unsigned nr_features;							//当前feature个数
+};
+
+
 
 #define SVC_NAME "vTopSrv"
 #define SVC_DISPLAYNAME "Halsign Tools for Virtual Machines Service"
@@ -140,7 +171,7 @@ void ServiceMain(int argc, char** argv)
 		ErrorLog("XenSvc: Unable to init xenservice");
 		return;
 	}
-
+	LaunchAppInSession("notepad");
 	WaitForSingleObject(_hServiceExitEvent, INFINITE);
 	ServiceControlManagerUpdate(0, SERVICE_STOPPED);
 	CloseHandle(_hServiceExitEvent);
@@ -215,4 +246,228 @@ void ServiceControlManagerUpdate(DWORD dwExitCode, DWORD dwState)
 	_ServiceStatus.dwWin32ExitCode = dwExitCode;
 	_ServiceStatus.dwCurrentState = dwState;
 	SetServiceStatus(_hStatus, &_ServiceStatus);
+}
+
+void Run()
+{
+	STARTUPINFOA					startInfo;
+	PROCESS_INFORMATION			processInfo;
+
+	InfoLog(__FUNCTION__" Guest agent main loop starting");
+
+	//GetWindowsVersion();
+
+	ZeroMemory(&startInfo, sizeof(startInfo));
+	ZeroMemory(&processInfo, sizeof(processInfo));
+	startInfo.cb = sizeof(startInfo);
+
+	//update performance information in the WMI repository
+	//allows developers and IT Administrators to create scripts
+	//that access performance information
+	//
+	// Refresh WMI ADAP classes
+	//
+	if (!CreateProcessA(
+		NULL,
+		"\"wmiadap\" /f",
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&startInfo,
+		&processInfo
+		))
+	{
+		WarnLog("XenSvc: Unable to refresh WMI ADAP: %d", GetLastError());
+	}
+	else
+	{
+		//Wait until child process exits.
+		WaitForSingleObject(processInfo.hProcess, 5000);
+
+		CloseHandle(processInfo.hProcess);
+		CloseHandle(processInfo.hThread);
+	}
+
+	//
+	// Enable disk counters forcibly so that we can retrieve disk performance
+	// using IOCTL_DISK_PERFORMANCE
+	//
+	if (!CreateProcessA(
+		NULL,
+		"\"diskperf\" -y",
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&startInfo,
+		&processInfo
+		))
+	{
+		WarnLog("XenSvc: Cannot enable disk perf counters: %d", GetLastError());
+	}
+	else
+	{
+		WaitForSingleObject(processInfo.hProcess, 5000);
+
+		CloseHandle(processInfo.hProcess);
+		CloseHandle(processInfo.hThread);
+	}
+
+
+}
+
+void AddFeature(struct watch_feature_set *wfs, 
+				const char *path,
+				const char *flag,
+				const char *name,
+				void(*handler)(void *), 
+				void *ctx)
+{
+	unsigned n;
+	if (wfs->nr_features == MAX_FEATURES) 
+	{
+		WarnLog("Too many features!", ERROR_INVALID_FUNCTION);
+		return;
+	}
+	n = wfs->nr_features;
+	//wfs->features[n].watch = EstablishWatch(path);
+	
+	if (wfs->features[n].watch == NULL) 
+	{
+		WarnLog("EstablishWatch() for AddFeature()");
+		return;
+	}
+	wfs->features[n].feature_flag = flag;
+	wfs->features[n].handler = handler;
+	wfs->features[n].ctx = ctx;
+	wfs->features[n].name = name;
+	wfs->nr_features++;
+}
+ void LaunchAppInSession(LPSTR lpCommand)
+{
+	DWORD						dwSessionId = 0;
+	HANDLE						hUserToken = NULL;
+	PROCESS_INFORMATION			pi;
+	STARTUPINFOA				si;
+	DWORD						dwRet = 0;
+	HANDLE						hPToken = NULL;
+	HANDLE						hUserTokenDup = NULL;
+	DWORD						dwCreationFlags;
+
+
+	dwSessionId = WTSGetActiveConsoleSessionId();
+
+	InfoLog(__FUNCTION__" Session Id %ul", dwSessionId);
+
+	do 
+	{
+		LUID luid;
+		TOKEN_PRIVILEGES tp;
+		//
+		//new process has a new console, instead of inheriting its parent's console (the default). 
+		dwCreationFlags = NORMAL_PRIORITY_CLASS | CREATE_NEW_CONSOLE;
+
+		WTSQueryUserToken(dwSessionId, &hUserToken);
+
+		ZeroMemory(&si, sizeof(STARTUPINFO));
+		si.cb = sizeof(STARTUPINFO);
+		//The system  creates the default input desktop for the interactive window station(Winsta0\default).
+		//Processes started by the logged - on user are associated with the Winsta0\default desktop.
+		//The name of the desktop, or the name of both the desktop and window station for this process. 
+		si.lpDesktop = "winsta0\\default";
+		ZeroMemory(&pi, sizeof(pi));
+		//opens the access token associated with a process.
+		if (!::OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY
+			| TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY | TOKEN_ADJUST_SESSIONID
+			| TOKEN_READ | TOKEN_WRITE, &hPToken))
+		{
+			dwRet = GetLastError();
+			WarnLog(__FUNCTION__" OpenProcessToken error, %x", dwRet);
+			break;
+		}
+		//retrieves the locally unique identifier (LUID) 
+		// used on a specified system to locally represent the specified privilege name.
+		if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &luid))
+		{
+			dwRet = GetLastError();
+			WarnLog(__FUNCTION__" LookupPrivilegeValue error, %x", dwRet);
+			break;
+		}
+
+		tp.PrivilegeCount = 1;
+		tp.Privileges[0].Luid = luid;
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+		//creates a new access token that duplicates an existing token
+		if (!DuplicateTokenEx(hPToken, MAXIMUM_ALLOWED, NULL, SecurityIdentification, TokenPrimary, &hUserTokenDup))
+		{
+			dwRet = GetLastError();
+			WarnLog(__FUNCTION__" DuplicateTokenEx error, %x", dwRet);
+			break;
+		}
+		//Adjust Token privilege
+		if (!SetTokenInformation(hUserTokenDup, TokenSessionId, (void*)&dwSessionId, sizeof(DWORD)))
+		{
+			dwRet = GetLastError();
+			WarnLog(__FUNCTION__" SetTokenInformation error, %x", dwRet);
+			break;
+		}
+		if (!AdjustTokenPrivileges(hUserTokenDup, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, NULL))
+		{
+			dwRet = GetLastError();
+			WarnLog(__FUNCTION__" AdjustTokenPrivileges error, %x", dwRet);
+			break;
+		}
+		LPVOID pEnv = NULL;
+		if (CreateEnvironmentBlock(&pEnv, hUserTokenDup, TRUE))
+		{
+			//the environment block pointed to by lpEnvironment uses Unicode characters.
+			dwCreationFlags |= CREATE_UNICODE_ENVIRONMENT;
+		}
+
+		// Launch the process in the client's logon session.
+		if (CreateProcessAsUserA(hUserTokenDup,    // client's access token
+			NULL,        // file to execute
+			lpCommand,        // command line
+			NULL,            // pointer to process SECURITY_ATTRIBUTES
+			NULL,            // pointer to thread SECURITY_ATTRIBUTES
+			FALSE,            // handles are not inheritable
+			dwCreationFlags,// creation flags
+			pEnv,          // pointer to new environment block
+			NULL,          // name of current directory
+			&si,            // pointer to STARTUPINFO structure
+			&pi            // receives information about new process
+			))
+		{
+			InfoLog(__FUNCTION__" CreateProcessAsUser %s Success", lpCommand);
+		}
+		else
+		{
+			dwRet = GetLastError();
+			WarnLog(" CreateProcessAsUser %s error, %x", lpCommand, dwRet);
+			break;
+		}
+	} while (0);
+	
+	if (NULL != hUserToken)
+	{
+		CloseHandle(hUserToken);
+	}
+	else;
+
+	if (NULL != hUserTokenDup)
+	{
+		CloseHandle(hUserTokenDup);
+	}
+	else;
+
+	if (NULL != hPToken)
+	{
+		CloseHandle(hPToken);
+	}
+
 }
